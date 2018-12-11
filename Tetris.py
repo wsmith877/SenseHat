d38111389a6d3a02fd827c330282587b2300c86c
wsmith877 <45600366+wsmith877@users.noreply.github.com>
2018-12-11 15:12:28 +0000

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -15,8 +15,9 @@ left = 0
 right = 0
 time = 100
 sleepvar = 0.5
-peice = 0
-peicedone = 1
+peice = 1
+start = 1
+peicedone = 0
 score = 0
 rotation = 0
 peicex = 0
@@ -26,7 +27,18 @@ peicey2 = 0
 color = 0
 peices = []
 mpeices = []
+mpeices2 = []
+bounds = [
+    [-1,0],[-1,1],[-1,2],[-1,3],[-1,4],[-1,5],[-1,6],[-1,7],
+    [8,0],[8,1],[8,2],[8,3],[8,4],[8,5],[8,6],[8,7],
+    [0,-1],[1,-1],[2,-1],[3,-1],[4,-1],[5,-1],[6,-1],[7,-1],
+    [0,8],[1,8],[2,8],[3,8],[4,8],[5,8],[6,8],[7,8]
+    ]
+good1 = 1
+good2 = 1
 while time > 0:
+    good1 = 1
+    good2 = 1
     time -= 1
     up = 0
     right = 0
@@ -44,7 +56,7 @@ while time > 0:
           if event.direction == "right":
              right = 1      # Right arrow
     """choosing random peice and color"""
-    if peicedone == 1:
+    if peicedone == 1 or start == 1:
         peice = 1
         color = randint(0,6)
         if color == 0:
@@ -74,14 +86,57 @@ while time > 0:
         rotation += 1
     if rotation == 2:
         rotation = 0
-    if rotation == 0 and peice == 1:
-        mpeices.clear
-        for block in mpeices:
-            block[0] += peicex2
-            block[1] += peicey2
+    """Collision"""
+    mpeices2 = mpeices
+    for block in mpeices2:
+        del block[2]
+        if left == 1:
+            block[0] -= 1
+        if right == 1:
+            block[0] += 1
+        for i in bounds:
+            if good1 == 1:
+                if block == i:
+                    good1 = 0
+        for i in peices:
+            if good1 == 1:
+                if block == i:
+                    good1 = 0
+        block[1] -= 1
+        for i in bounds:
+            if good2 == 1:
+                if block == i:
+                    good2 = 0
+        for i in peices:
+            if good2 == 1:
+                if block == i:
+                    good2 = 0
+    for block in mpeices:
+        if good1 == 1:
+            if left == 1:
+                block[0] -= 1
+            if right == 1:
+                block[0] += 1
+        if good2 == 1:
+            block[1] += 1
+        else:
+            peicedone = 1
     peicex2 = 0
     peicey2 = 0
+    """adding block to "global" peices"""
+    if peicedone == 1:
+        peices.append[mpeices[0]]
+        del mpeices[0]
+        peices.append[mpeices[0]]
+        del mpeices[0]
+        peices.append[mpeices[0]]
+        del mpeices[0]
+        peices.append[mpeices[0]]
+        del mpeices[0]
     """starting peice"""
+    if start == 1:
+        start = 0
+        peicedone = 1
     if peice == 1 and peicedone == 1:
         peicex = 4
         peicey = 7
